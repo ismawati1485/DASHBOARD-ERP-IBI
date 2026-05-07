660a7a8f432623364a7f9292146d9d6e71e6b658
ismawati1485 <ismawatiainur@gmail.com>
2026-05-07 13:56:09 +0700

    change port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    server: {
+      port: 5173,
+    },
+  },
+
   tanstackStart: {
     server: { entry: "server" },
   },
